875809bbfe323bf304f56bf849a18cb8b9fb12bc
Kumar <Kumarkodi2001@gmail.com>
2022-03-31 14:37:09 +0530

    Shopping Kart Website

---
+++ Shopping Kart/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2021-22 Brought To You By Shopping Kart</strong>
+</footer>
